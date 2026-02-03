a46458c45e800429a9b5600382bfec3b12a1f4d8
Sanjoli06 <goyalsanjoli036@gmail.com>
2026-02-04 00:53:00 +0530

    Private Folders with _ and public folder with _URL using %5F

---
+++ app/(application)/dashboard/_components/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ComponentPage() {
+  return (
+    <div>
+      Components Page
+    </div>
+  )
+}
--- app/(application)/dashboard/settings/page.js
+++ app/(application)/dashboard/settings/page.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ComponentPage from '../_components/page'
 
 export default function Settings() {
   return (
     <div>
       Settings Page of Dashboard
+      <ComponentPage />
     </div>
   )
 }
